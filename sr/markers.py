2d9c7f51a0515325d6c7952235d2d247aaa9800c
Alastair Lynn <arplynn@gmail.com>
2014-07-22 18:32:53 +0100

    Differentiate between the two token classes

--- sr/markers.py
+++ sr/markers.py
@@ -3,14 +3,15 @@ from vision import create_marker_info_by_type, MARKER_TOKEN_SILVER, MARKER_TOKEN
 
 class Token(GameObject):
     grabbable = True
-    GOLD = 'GOLD'
-    SILVER = 'SILVER'
+    GOLD = 'gold'
+    SILVER = 'silver'
 
     def __init__(self, arena, colour, number):
         GameObject.__init__(self, arena)
         self.marker_info = create_marker_info_by_type({self.GOLD: MARKER_TOKEN_GOLD,
                                                        self.SILVER: MARKER_TOKEN_SILVER}[colour], number)
         self.grabbed = False
+        self.colour = colour
 
     def grab(self):
         self.grabbed = True
@@ -20,7 +21,7 @@ class Token(GameObject):
 
     @property
     def surface_name(self):
-        return 'sr/token{0}.png'.format('_grabbed' if self.grabbed else '')
+        return 'sr/token_{1}{0}.png'.format('_grabbed' if self.grabbed else '', self.colour)
 
 class WallMarker(GameObject):
     surface_name = 'sr/wall_marker.png'
